90db965b4307de81b6c019a1416852255daacca0
unknown <jair.lopes@fatec.sp.gov.br>
2021-09-02 22:47:04 -0300

    Any vs Unknown

---
+++ AULA-08-ANY-VS-UNKNOWN/dist/aula08.js
@@ -0,0 +1,11 @@
+"use strict";
+// Any vs Unknown
+// Por padrão o metodo add(x, y) possui os tipos any, que devem ser evitados por que podem ser qualquer tipo.
+// O tipo Unknown é um pouco mais seguro que o any por que é obrigado a fazer uma condicional para o codigo funcionar e também o mesmo nos informa qual os possiveis tipos irá retornar.
+function add(x, y) {
+    if (typeof x === "number" && typeof y === "number")
+        return x + y;
+    return null;
+}
+var teste = add(3, 5);
+console.log(teste);

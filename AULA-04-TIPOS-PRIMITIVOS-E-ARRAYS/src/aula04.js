b6205a399b14085666d7c2c5256869f733c84b29
unknown <jair.lopes@fatec.sp.gov.br>
2021-09-02 21:06:46 -0300

    Tipos primitivos e array

---
+++ AULA-04-TIPOS-PRIMITIVOS-E-ARRAYS/src/aula04.js
@@ -0,0 +1,18 @@
+"use strict";
+// Tipos Primitivos
+var msg = "Teste de string";
+console.log(msg);
+var n = 12;
+console.log(n);
+var isValid = false;
+console.log(isValid);
+// Arrays
+var minhaArray;
+minhaArray = [1, 2, 3];
+console.log(minhaArray);
+var arrayString;
+arrayString = ["a", "b", "c"];
+console.log(arrayString);
+var arrayStringGeneric;
+arrayStringGeneric = ["teste", "teste2"];
+console.log(arrayStringGeneric);
---
+++ AULA-04-TIPOS-PRIMITIVOS-E-ARRAYS/src/aula04.ts
@@ -0,0 +1,33 @@
+// Tipos Primitivos
+
+let msg: string = "Teste de string";
+
+console.log(msg);
+
+let n: number = 12;
+
+console.log(n);
+
+let isValid: boolean = false;
+
+console.log(isValid);
+
+// Arrays
+
+let minhaArray: number[];
+
+minhaArray = [1, 2, 3];
+
+console.log(minhaArray)
+
+let arrayString: string[];
+
+arrayString = ["a", "b", "c"];
+
+console.log(arrayString);
+
+let arrayStringGeneric: Array<string>;
+
+arrayStringGeneric = ["teste", "teste2"];
+
+console.log(arrayStringGeneric);

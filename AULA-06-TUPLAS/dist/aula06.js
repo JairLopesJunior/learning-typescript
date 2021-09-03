4158a717c994624dfa1cc6b6b0baae1764874d04
unknown <jair.lopes@fatec.sp.gov.br>
2021-09-02 22:19:35 -0300

    Tuplas

---
+++ AULA-06-TUPLAS/dist/aula06.js
@@ -0,0 +1,7 @@
+"use strict";
+// TUPLAS - Utilizado quando tem um array de tipos especificos.
+var aluno2;
+aluno2 = {
+    notas: [1, 2, 3, 4]
+};
+console.log(aluno2);
---
+++ AULA-06-TUPLAS/src/aula06.ts
@@ -0,0 +1,9 @@
+// TUPLAS - Utilizado quando tem um array de tipos especificos.
+
+let aluno2: { notas: [number, number, number, number] };
+
+aluno2 = {
+    notas: [1, 2, 3, 4]
+}
+
+console.log(aluno2)

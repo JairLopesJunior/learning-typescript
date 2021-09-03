e11dc19e41a802ab0b09681e8bbadcc88d27c641
unknown <jair.lopes@fatec.sp.gov.br>
2021-09-02 22:08:51 -0300

    Objetos

---
+++ AULA-05-OBJETOS/src/aula05.ts
@@ -0,0 +1,10 @@
+let aluno: {nome: string, aprovado: boolean, idade: number, notas: number[]};
+
+// Campos opcionais em um objeto Ex: { aprovado?: boolean }
+
+aluno = {
+    nome: "Jair",
+    aprovado: true,
+    idade: 28,
+    notas: [1, 2, 3, 7]
+}

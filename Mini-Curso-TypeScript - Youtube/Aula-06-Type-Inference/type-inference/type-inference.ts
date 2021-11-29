10523489ce83735f778384e4f44397fc002e3b36
Jair Lopes Junior <47534815+JairLopesJunior@users.noreply.github.com>
2021-11-29 15:30:44 -0300

    Add files via upload

---
+++ Mini-Curso-TypeScript - Youtube/Aula-06-Type-Inference/type-inference/type-inference.ts
@@ -0,0 +1,8 @@
+// Type Inference
+let message2 = "Mensagem";
+
+window.addEventListener("click", (e) => {
+  console.log(e.target);
+});
+
+// O evento do metodo acima esta tipado como MouseEvent, porem não é necessário colocar.

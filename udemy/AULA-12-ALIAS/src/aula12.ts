df03d93ad51a8fd9e07a2ca179e19e9b1f47cb8a
Jair Lopes Junior <47534815+JairLopesJunior@users.noreply.github.com>
2021-11-29 16:46:58 -0300

    Update aula12.ts

--- udemy/AULA-12-ALIAS/src/aula12.ts
+++ udemy/AULA-12-ALIAS/src/aula12.ts
@@ -1,2 +1,30 @@
-// Alias
+// Ex 1:
+type testeAlias = number | string | boolean;
 
+let teste: testeAlias;
+teste = 10;
+
+
+
+// Ex 2:
+type User = {
+  nome: string,
+  idade?: number
+}
+
+const jair: User = {
+  nome: "Jair"
+}
+
+const maria: User = {
+  nome: "Maria",
+  idade: 30
+}
+
+
+
+// Ex 3:
+type Sizes = "12px" | "18px" | "24px";
+
+const small: Sizes = "12px";
+const normal: Sizes = "18px";
